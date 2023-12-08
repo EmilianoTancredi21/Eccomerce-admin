c9dbe091be652d39d90c438b9c10622bb27836f1
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2023-12-08 15:00:26 -0300

    Se agrega sistema de notificaciones (React hot toast)

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -17,29 +17,47 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
+// Define el esquema de validación del formulario utilizando Zod
 const formSchema = z.object({
   name: z
     .string()
-    .min(5, { message: "El nombre debe tener al menos 5 caracteres." }),
+    .min(5, { message: "El nombre debe tener al menos 5 caracteres." }), // El campo "name" debe ser una cadena de al menos 5 caracteres
 });
 
+// Componente funcional que representa el modal para crear una tienda
 export const StoreModal = () => {
-  const storeModal = useStoreModal();
+  const storeModal = useStoreModal(); // Obtiene el estado y las funciones del modal desde el hook useStoreModal
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false); // Inicializa el estado local "loading" con el valor inicial de "false"
 
+  // Crea un formulario controlado utilizando el hook useForm de react-hook-form
   const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+    resolver: zodResolver(formSchema), // Utiliza el resolver zod para la validación del formulario
     defaultValues: {
-      name: "",
+      name: "", // Establece el valor inicial del campo "name" como una cadena vacía
     },
   });
 
+  // Función que se ejecuta cuando se envía el formulario
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      setLoading(true); // Establece el estado de "loading" como "true"
+
+      // Realiza una solicitud POST a "/api/stores" utilizando Axios con los valores del formulario
+      const response = await axios.post("/api/stores", values);
+
+      toast.success("La tienda se creó correctamente."); // Muestra un mensaje de éxito utilizando la librería toast
+    } catch (error) {
+      toast.error("Algo salió mal..."); // Muestra un mensaje de error utilizando la librería toast
+    } finally {
+      setLoading(false); // Restablece el estado de "loading" a "false"
+    }
   };
 
+  // Renderiza el componente Modal que muestra un formulario para crear una tienda
   return (
     <Modal
       title="Crear tienda"
